ab77b07e50f517c866bf26a24aaba1565410e567
dehemi weerakoon <115975647+dehemiweerakoon@users.noreply.github.com>
2025-04-22 15:40:49 +0530

    add home page

---
+++ src/page/Home/Home.tsx
@@ -0,0 +1,34 @@
+import { motion } from "motion/react";
+import { LampContainer } from "../../components/ui/lamp";
+
+export function LampDemo() {
+  return (
+    <LampContainer>
+      <motion.h1
+        initial={{ opacity: 0.5, y: 100 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{
+          delay: 0.3,
+          duration: 0.8,
+          ease: "easeInOut",
+        }}
+        className="mt-8 bg-gradient-to-br from-slate-300 to-slate-500 py-4 bg-clip-text text-center text-4xl font-medium tracking-tight text-transparent md:text-7xl"
+      >
+        Customized Polls
+        <br />
+        Right Away
+      </motion.h1>{" "}
+      <button className="text-violet-100">Create a Poll</button>
+    </LampContainer>
+  );
+}
+
+const Home = () => {
+  return (
+    <>
+      <LampDemo />
+    </>
+  );
+};
+
+export default Home;
