1cc8ca01867851b34fabb79a69f4e9c87a8b9e19
dehemi weerakoon <115975647+dehemiweerakoon@users.noreply.github.com>
2025-04-24 23:40:02 +0530

    Added poll submit and creation

--- src/page/Poll/Poll.tsx
+++ src/page/Poll/Poll.tsx
@@ -122,8 +122,8 @@ export default function Poll() {
       <select
         id="underline_select"
         className="block py-2.5 px-5 text-sm text-gray-500 bg-transparent border-0 border-b-2 border-gray-200 appearance-none dark:text-gray-400 dark:border-gray-700 focus:outline-none focus:ring-0 focus:border-gray-200 peer"
-
-        onChange={(e) => setSelectedOption(Number(e.target.value))}
+      
+        onMouseDown={(e) => setSelectedOption(Number((e.target as HTMLSelectElement).value))}
       >
         {polls.map((poll) => (
           <option key={poll.id} value={poll.id}>
